0b07019b24e47f5342112b977c024665e40560af
3120weissee <jackweisse@gmail.com>
2017-05-16 12:22:41 -0500

    Adjusting map

--- script/map.js
+++ script/map.js
@@ -1,27 +1,30 @@
 var world = [[-1,-1,-1,-1,-1,-1],
-		   [-1, 0, 0, 0, 0,-1],
-		   [-1, 0, 0, 0, 0,-1],
-		   [-1, 0, 0, 0, 0,-1],
-		   [-1, 0, 0, 0, 0,-1],
+		   [-1, 1, 1, 1, 1,-1],
+		   [-1, 1, 1, 1, 1,-1],
+		   [-1, 1, 1, 1, 1,-1],
+		   [-1, 1, 1, 1, 1,-1],
 		   [-1,-1,-1,-1,-1,-1]];
 
-function displayWorld() {
-	var output = "";
-	for(var i=0; i < world.length; i++) {
-		output += "<div class='row'>";
-		for(var j = 0; j < world[i].length; j++) {
-			if(world[i][j] == -1) { //wall
-				output += "<div class='wall'></div>";
-			} else if(world[i][j] == 1) { //dot
-				output += "<div class='dot'></div>";
-			} else if(world[i][j] == 0) { //empty
-				output += "<div class='empty'></div>";
+$(document).ready(function() {
+	function displayWorld() {
+		var output = "";
+		for(var i=0; i < world.length; i++) {
+			output += "<div class='row'>";
+			for(var j = 0; j < world[i].length; j++) {
+				if(world[i][j] == -1) { //wall
+					output += "<div class='wall'></div>";
+				} else if(world[i][j] == 1) { //dot
+					output += "<div class='dot'></div>";
+				} else if(world[i][j] == 0) { //empty
+					output += "<div class='empty'></div>";
+				}
 			}
+			output += "</div>";
 		}
-		output += "</div>";
+		$("#world").html(output);
 	}
-	$("#world").html(output);
-}
 
-displayWorld();
+	displayWorld();
+});
+
 
