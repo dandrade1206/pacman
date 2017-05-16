301cd3fecf2cef234eba60caac9f6dd92e58b5fe
Tom <Tom Hewuse>
2017-05-16 13:31:20 -0500

    working map

--- script/map.js
+++ script/map.js
@@ -1,15 +1,17 @@
-var world = [[-1,-1,-1,-1,-1,-1],
-		   [-1, 0, 0, 0, 0,-1],
-		   [-1, 0, 0, 0, 0,-1],
-		   [-1, 0, 0, 0, 0,-1],
-		   [-1, 0, 0, 0, 0,-1],
-		   [-1,-1,-1,-1,-1,-1]];
-var packman={
-	x:0,
-	y:0,
-}
 
 $(document).ready(function(){
+    var world = [[-1,-1,-1,-1,-1,-1],
+				[-1, 1, 1, 1, 1,-1],
+				[-1, 0, 0, 0, 0,-1],
+				[-1, 0, 0, 0, 0,-1],
+				[-1, 0, 0, 0, 0,-1],
+				[-1,-1,-1,-1,-1,-1]];
+
+    var packman={
+        x:0,
+        y:0,
+    };
+
 	function displayWorld() {
 		var output = "";
 		for(var i=0; i < world.length; i++) {
@@ -30,7 +32,7 @@ $(document).ready(function(){
 	}
 
 	function displayPacman(){
-		document.getElementById('pacman').style.tom=packman.y*20+"px";
+		document.getElementById('pacman').style.top=packman.y*20+"px";
 		document.getElementById('pacman').style.left=packman.x*20+"px";
 	}
 
